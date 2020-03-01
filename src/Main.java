caa6591511407ad4189abd6645eceeac920e247c
Dexlet <51440533+Dexlet@users.noreply.github.com>
2020-03-01 21:52:41 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 			//new SimpleSubscriber(),
 			//new ReallySimpleSubscriber(),
 			new Lucas_Series(),
+			new Summation_Series (),
 	};
 	public static void main(String[] args) {
 		Topic mathTopic = new Topic();
