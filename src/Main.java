4f35d8caf1f3debd59f648025c6846d19f8b398e
Aya-Shehata <61581316+Aya-Shehata@users.noreply.github.com>
2020-03-03 19:01:39 +0200

    20170068

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,10 @@ public class Main {
 			//new SimpleSubscriber(),
 			//new ReallySimpleSubscriber(),
 			new Lucas_Series(),
+			new fibonacci(),/*20170129*/
+			new SphereVolume(),
+			new Summation_Series (),
+			new CircleCircumference()
 		new CircleArea(),
 	};
 	public static void main(String[] args) {
